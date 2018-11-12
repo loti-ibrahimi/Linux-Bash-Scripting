6b12c2fe2d06c22775c7f9b7b6d71015892b3e78
Loti Ibrahimi <loti.ibrahimi@outlook.com>
2018-11-12 02:42:46 +0000

    Arrays

---
+++ Scripts2/4currentDirectory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Loti Ibrahimi (20015453) | Internet of Things Year 3
+
+# Shell Script to list all files of current directory having read & write permission to the user.
+
+ls -l | grep rw
